13f4181b474337c7338b912dc363e75db0271d3b
eliotjoy <105752204+eliotjoy@users.noreply.github.com>
2022-08-31 15:55:58 -0500

    Add files via upload

---
+++ PersonCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PersonCard = (props) => {
+    const { firstName, lastName, age, hairColor } = props;
+    return (
+        <div className="personCard">
+            <h2>{ lastName }, { firstName }</h2>
+            <p>Age: { age }</p>
+            <p>Hair Color: { hairColor }</p>
+        </div>
+    );
+}
+export default PersonCard;
