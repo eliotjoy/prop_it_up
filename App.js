be7e723e86cd8513dd14f901a21c85d0b7ec4cd3
eliotjoy <105752204+eliotjoy@users.noreply.github.com>
2022-08-31 15:55:37 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import "./App.css";
+import PersonCard from "./components/PersonCard"
 
-const PersonCard = (props) => {
-  const { firstName, lastName, age, hairColor } = props;
+function App() {
   return (
-    <div className="personCard">
-        <h2>{ lastName }, { firstName }</h2>
-        <p>Age: { age }</p>
-        <p>Hair Color: { hairColor }</p>
-        <PersonCard firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"} />
-    </div>
-  );
+    <>
+      <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black" />
+      <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown" />
+      <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown" />
+      <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown" />
+    </>
+  )
 }
-export default PersonCard;
+
+export default App;
